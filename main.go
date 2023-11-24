98f70319452fda5a8dfae4e5e6bb2372111d0cf0
bmc-eng <bclarkeuk@googlemail.com>
2023-11-24 14:17:30 +0000

    split string input fixed

--- main.go
+++ main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
 func wordsGameHandler(c *gin.Context) {
+	// Split the letters into an array
+	strLetters := c.Param("letters")
+	letters := strings.Split(strLetters, ";")
 
-	c.JSON(http.StatusOK, gin.H{"test": "ok"})
+	c.JSON(http.StatusOK, gin.H{"test": letters})
 }
 
 func main() {
