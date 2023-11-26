0abaff86e359672f6f7b6645d4e23ad603e65849
bmc-eng <bclarkeuk@googlemail.com>
2023-11-26 21:32:39 +0000

    pull in the dictionary json keys into an array

--- main.go
+++ main.go
@@ -1,18 +1,37 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"os"
 	"strings"
 
 	"github.com/gin-gonic/gin"
 )
+
+var dictionary map[string]interface{}
+var words []string
 
 func wordsGameHandler(c *gin.Context) {
 	// Split the letters into an array
 	strLetters := c.Param("letters")
 	letters := strings.Split(strLetters, ";")
 
-	c.JSON(http.StatusOK, gin.H{"test": letters})
+	c.JSON(http.StatusOK, gin.H{"test": letters, "dictionary": words[0]})
+}
+
+func init() {
+	//unpack the json file
+	file, _ := os.ReadFile("data/dictionary.json")
+	_ = json.Unmarshal([]byte(file), &dictionary)
+
+	// pull all the keys into a single words array list
+	words = make([]string, 0, len(dictionary))
+	for k := range dictionary {
+		words = append(words, k)
+	}
+	fmt.Println("setup complete")
 }
 
 func main() {
